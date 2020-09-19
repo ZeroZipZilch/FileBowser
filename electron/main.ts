4c2dbf7342f5a4717f4cb4440ff4e194046c24b1
Moha Elo <moha.elo@gmail.com>
2020-09-19 17:26:39 +0200

    Hid menu and fixed so prod would work

--- electron/main.ts
+++ electron/main.ts
@@ -1,10 +1,12 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
 
 let mainWindow: Electron.BrowserWindow | null;
 
 function createWindow() {
+  Menu.setApplicationMenu(null);
+
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -16,12 +18,13 @@ function createWindow() {
 
   if (process.env.NODE_ENV === 'development') {
     mainWindow.loadURL(`http://localhost:4000`);
+    mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadURL(
       url.format({
-          pathname: path.join(__dirname, '../index.html'),
-          protocol: 'file:',
-          slashes: true
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true,
       })
     );
   }
